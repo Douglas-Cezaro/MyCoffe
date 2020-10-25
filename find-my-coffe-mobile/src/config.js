70a92b4413f8b0bb9c17e144767da3bd43c0749a
douglas-cezaro <cezarodouglas@gmail.com>
2020-10-24 23:40:31 -0300

    Ajustes

--- find-my-coffe-mobile/src/config.js
+++
@@ -1,1 +0,0 @@
-exports.REACT_APP_GOOGLE_KEY = "AIzaSyAGrb1b4dVBlsOBe0tvx5MtcriMEFG8ok4";
--- find-my-coffe-mobile/src/services/api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-const Api = axios.create({ baseURL: "http://4bfbc2334d8b.ngrok.io/api/v1" });
-
-export default Api;
